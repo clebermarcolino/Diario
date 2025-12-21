b7492db4dbc5fd30fd2bdb523ec418a0f625544f
clebermarcolino <clebermarcolinogba@gmail.com>
2025-12-21 00:59:17 -0300

    arredondamentos de botões em pessoa.html e mudança na barra de navegação em escrever.html

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\cleber.marcolino\Documents\Diario\venv"
+set "VIRTUAL_ENV=C:\Users\clebe\.vscode\cli\Diario\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\cleber.marcolino\Documents\Diario\venv'
+set -gx VIRTUAL_ENV 'C:\Users\clebe\.vscode\cli\Diario\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
